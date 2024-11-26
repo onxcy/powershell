ad531add5d79b7b0156420c2803c3ff92123c2b7
onxcy <174684063+onxcy@users.noreply.github.com>
2024-11-26 16:12:20 +0100

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,39 @@
 use std::process::{Command, Output};
 
-use base64::prelude::*;
+pub struct PowerShell {
+    buf: Vec<String>,
+}
 
-pub fn execute(command: impl AsRef<str>) -> Output {
-    Command::new("powershell")
-        .arg("-NoLogo")
-        .arg("-NoProfile")
-        .arg("-NonInteractive")
-        .arg("-ExecutionPolicy")
-        .arg("Bypass")
-        .arg("-EncodedCommand")
-        .arg(
-            BASE64_STANDARD.encode(bytemuck::cast_slice::<u16, u8>(
-                command
-                    .as_ref()
-                    .encode_utf16()
-                    .collect::<Vec<u16>>()
-                    .as_slice(),
-            )),
-        )
-        .output()
-        .unwrap()
+impl PowerShell {
+    pub fn new() -> Self {
+        Self { buf: Vec::new() }
+    }
+
+    pub fn no_confirm(mut self) -> Self {
+        self.buf.push(
+            "$ConfirmPreference=[System.Management.Automation.ConfirmImpact]::None".to_string(),
+        );
+        self
+    }
+
+    pub fn add_variable(mut self, name: &str, value: &str) -> Self {
+        self.buf.push(format!("${}='{}'", name, value));
+        self
+    }
+
+    pub fn invoke(mut self, command: &str) -> Output {
+        self.buf.push(command.to_string());
+        use base64::prelude::*;
+        let encoded = BASE64_STANDARD.encode(bytemuck::cast_slice::<u16, u8>(
+            &self.buf.join("\n").encode_utf16().collect::<Vec<_>>(),
+        ));
+        Command::new("PowerShell")
+            .arg("-NoLogo")
+            .arg("-NoProfile")
+            .arg("-NonInteractive")
+            .arg("-EncodedCommand")
+            .arg(encoded)
+            .output()
+            .unwrap()
+    }
 }
